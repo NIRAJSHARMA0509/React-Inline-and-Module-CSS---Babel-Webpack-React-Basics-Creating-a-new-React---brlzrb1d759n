40806339c600c896d9cb543be1e39dfeceef4749
NIRAJSHARMA0509 <120631030+NIRAJSHARMA0509@users.noreply.github.com>
2022-12-20 16:19:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,13 @@ import '../styles/App.css';
 import styles from '../styles/appStyles.module.css';
 const App = () => {
 const heading ={
-
-
+fontSize: "50px",
+color:"red",
 }
   return (
     <div id="main">
       <h1 className={styles.success}>Success</h1>
-      <h1 id='error' style={{ color: 'red', font-size: 50px }}">Error</h1>
+      <h1 id='error'style={heading}>Error</h1>
     </div>
   )
 }
